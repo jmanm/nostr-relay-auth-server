918da7a5bb14fd345b6374b27915be3a7c7f11fa
Jamin M <jaminmenter@outlook.com>
2023-04-12 22:42:31 -0700

    read allowed kinds/authors from config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::vec;
+use std::{vec, fs, path};
 
 use nostr_sdk::prelude::*;
+use toml::Table;
 use tonic::{transport::Server, Request, Response, Status};
 
 use nauthz_grpc::authorization_server::{Authorization, AuthorizationServer};
@@ -66,13 +67,52 @@ impl Authorization for EventAuthz {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
+    let mut allowed_kinds = vec![0, 1, 2, 3, 30023];
+    let mut allowed_authors = Vec::new();
+
+    let path = path::Path::new("./config.toml");
+    if path.exists() {
+        if let Ok(content) = fs::read_to_string(path) {
+            println!("Loaded config from config.toml");
+
+            let table = content
+                .parse::<Table>()
+                .expect("Invalid config file");
+
+            let config = table
+                .get("config")
+                .unwrap()
+                .as_table()
+                .unwrap();
+
+            if let Some(kinds) = config.get("allowed-kinds") {
+                allowed_kinds = kinds
+                    .as_array()
+                    .unwrap()
+                    .iter()
+                    .map(|k| { k.as_integer().unwrap() as u64 })
+                    .collect();
+            }
+
+            if let Some(authors) = config.get("allowed-authors") {
+                allowed_authors = authors
+                    .as_array()
+                    .unwrap()
+                    .iter()
+                    .map(|a| { String::from(a.as_str().unwrap()) })
+                    .collect();
+            }
+        }
+    }
 
     let checker = EventAuthz {
-        allowed_kinds: vec![0, 1, 2, 3, 30023],
-        allowed_authors: vec!["<pub key>".to_string()],
+        allowed_kinds,
+        allowed_authors,
     };
     
+    let port: u32 = 50051;
+    let addr = format!("[::1]:{}", port).parse().unwrap();
+
     println!("EventAuthz Server listening on {}", addr);
     
     // Start serving
